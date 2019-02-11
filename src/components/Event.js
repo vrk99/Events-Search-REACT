fb864a0bb1f237e297b70f12b5f1aae50eeed784
Virik <vrkgajani@gmail.com>
2019-02-11 21:20:37 +0530

    Add Map on Event page

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
+const google = window.google;
+
+function Map(props) {
+  return (
+    <div id='map-canvas' style={{ height: '500px', width: '500px' }}>
+    </div>
+  )
+}
+
 class Event extends Component {
   constructor(props) {
     super(props);
@@ -18,7 +27,21 @@ class Event extends Component {
         console.log(this.state.event.venue_id)
         fetch("https://www.eventbriteapi.com/v3/venues/" + this.state.event.venue_id + "?token=YOUR_EVENTBRITE_OAUTH_TOKEN_HERE")
           .then(res => res.json())
-          .then(eventLocation => this.setState(() => ({ eventLocation })))
+          .then(eventLocation => {
+            this.setState(() => ({ eventLocation }));
+            const latitude = parseFloat(this.state.eventLocation.address.latitude), longitude = parseFloat(this.state.eventLocation.address.longitude);
+            const location = { lat: latitude, lng: longitude }
+            var map = new google.maps.Map(document.getElementById('map-canvas'), {
+              zoom: 12,
+              center: location,
+            });
+            const marker = new google.maps.Marker({
+              position: location,
+              map: map,
+              label: this.state.eventLocation.name,
+            })
+            this.forceUpdate();
+          })
           .catch(err => console.log(err))
       })
       .catch(err => console.log(err))
@@ -27,8 +50,7 @@ class Event extends Component {
   render() {
     return (
       <div>
-        {console.log(this.state.event)}
-        {console.log(this.state.eventLocation)}
+        <Map />
       </div>
     )
   }
