39a1e4eda95910021497575deb161d99619634da
Virik <vrkgajani@gmail.com>
2019-02-11 22:32:17 +0530

    Populate Event page

--- src/components/Event.js
+++ src/components/Event.js
@@ -4,7 +4,7 @@ const google = window.google;
 
 function Map(props) {
   return (
-    <div id='map-canvas' style={{ height: '500px', width: '500px' }}>
+    <div id='map-canvas' style={{ height: '400px', width: '400px' }}>
     </div>
   )
 }
@@ -24,7 +24,6 @@ class Event extends Component {
       .then(res => res.json())
       .then(event => {
         this.setState(() => ({ event }));
-        console.log(this.state.event.venue_id)
         fetch("https://www.eventbriteapi.com/v3/venues/" + this.state.event.venue_id + "?token=YOUR_EVENTBRITE_OAUTH_TOKEN_HERE")
           .then(res => res.json())
           .then(eventLocation => {
@@ -49,8 +48,26 @@ class Event extends Component {
 
   render() {
     return (
-      <div>
-        <Map />
+      <div className="container-fluid">
+      {console.log(this.state.event)}
+        {!(Object.entries(this.state.event).length === 0 && this.state.event.constructor === Object) && (
+          <div className="row">
+            <div className="col-md-9">
+              <div>
+                {this.state.event.logo && <img className="thumbnail" src={this.state.event.logo.url} alt="No Image Available" />}
+                <h2>{this.state.event.name.text}</h2>
+                <p id={this.state.event.id + "_description"}>{this.state.event.description.text}</p>
+                <p>Timezone: {this.state.event.start.timezone}</p>
+                <p>Start: {this.state.event.start.local}</p>
+                <p>End: {this.state.event.end.local}</p>
+                <p><em>For more information and bookings, <a href={"https://www.eventbrite.com/event/" + this.state.event.id}>Click Here</a></em></p>
+              </div>
+            </div>
+            <div className="col-md-3">
+              <Map />
+            </div>
+        </div>
+        )}
       </div>
     )
   }
